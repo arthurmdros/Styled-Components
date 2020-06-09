d84a582592e5954c301ab2d992886413b7bb169e
arthurmdros <arthurmedeiros2005@gmail.com>
2020-06-09 01:38:22 -0300

    Adicionando novos componentes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+import { Title, SmallTitle } from './styles';
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <Title fontSize={20}>
+          Hello World
+          <span> de um mundo distante.</span>
+        </Title>
+
+        <SmallTitle>Mas não tão distante assim.</SmallTitle>
+      </div>
+    );
+  }
 }
 
 export default App;
